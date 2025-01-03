5b93992072ffee9cc81d13a4f487a031afa0562f
luke <108032666+kejjjjj@users.noreply.github.com>
2025-01-03 23:38:37 +0200

    Add project files.

---
+++ cg/cg_offsets.hpp
@@ -0,0 +1,6 @@
+
+struct clipMap_t;
+struct cmd_function_s;
+
+inline clipMap_t* cm = reinterpret_cast<clipMap_t*>(0x1F41A00);
+inline cmd_function_s** cmd_functions = reinterpret_cast<cmd_function_s**>(0x1F416F4);
